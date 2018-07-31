48975b00659620b1aa81f5ca8dde0068a95fecb9
Jakub Josef <jakub.josef@gmail.com>
2018-07-31 13:53:44 +0200

    Added export of WidgetLibraryService

--- projects/angular6-json-schema-form/src/public_api.ts
+++ projects/angular6-json-schema-form/src/public_api.ts
@@ -6,9 +6,9 @@ export * from './lib/json-schema-form.service';
 export * from './lib/json-schema-form.component';
 export * from './lib/framework-library';
 export * from './lib/widget-library';
+export * from './lib/widget-library/widget-library.service';
 export * from './lib/shared';
 export * from './lib/framework-library/material-design-framework/material-design-framework.module';
 export * from './lib/framework-library/bootstrap-3-framework/bootstrap-3-framework.module';
 export * from './lib/framework-library/bootstrap-4-framework/bootstrap-4-framework.module';
 export * from './lib/framework-library/no-framework/no-framework.module';
-
