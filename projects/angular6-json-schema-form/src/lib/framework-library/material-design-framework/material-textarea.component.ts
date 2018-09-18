3c7961bd54759de6e7c00a86ae7f556836c433f3
Richard Cox <richard.cox@suse.com>
2018-09-18 16:41:38 +0100

    Fix ::ng-deep bleed into parent components

--- projects/angular6-json-schema-form/src/lib/framework-library/material-design-framework/material-textarea.component.ts
+++ projects/angular6-json-schema-form/src/lib/framework-library/material-design-framework/material-textarea.component.ts
@@ -51,7 +51,7 @@ import { JsonSchemaFormService } from '../../json-schema-form.service';
       [innerHTML]="options?.errorMessage"></mat-error>`,
   styles: [`
     mat-error { font-size: 75%; margin-top: -1rem; margin-bottom: 0.5rem; }
-    ::ng-deep mat-form-field .mat-form-field-wrapper .mat-form-field-flex
+    ::ng-deep json-schema-form mat-form-field .mat-form-field-wrapper .mat-form-field-flex
       .mat-form-field-infix { width: initial; }
   `],
 })
