ea2b03201000f584d6d5f921056b689bbef7c8f5
pouriam <pouria.mahmoudi@gmail.com>
2019-04-05 11:08:56 -0700

    Update index.ts

--- projects/angular6-json-schema-form/src/lib/framework-library/index.ts
+++ projects/angular6-json-schema-form/src/lib/framework-library/index.ts
@@ -1,5 +1,7 @@
 export { FrameworkLibraryService } from './framework-library.service';
 
+export { Framework } from './framework';
+
 export { NoFramework } from './no-framework/no.framework';
 export { NoFrameworkModule } from './no-framework/no-framework.module';
 
