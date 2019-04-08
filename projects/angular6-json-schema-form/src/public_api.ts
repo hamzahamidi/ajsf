2c405d09a092ceb150b4a94fa2e42793769dbaa7
pouriam <pouria.mahmoudi@gmail.com>
2019-04-08 15:17:01 -0700

    Update public_api.ts
    export JsonSchemaFormComponent, WidgetLibraryModule to be available outside the library. Needs for adding a framework module.

--- projects/angular6-json-schema-form/src/public_api.ts
+++ projects/angular6-json-schema-form/src/public_api.ts
@@ -11,4 +11,5 @@ export * from './lib/framework-library/material-design-framework/material-design
 export * from './lib/framework-library/bootstrap-3-framework/bootstrap-3-framework.module';
 export * from './lib/framework-library/bootstrap-4-framework/bootstrap-4-framework.module';
 export * from './lib/framework-library/no-framework/no-framework.module';
-
+export * from './lib/json-schema-form.module';
+export * from './lib/widget-library/widget-library.module';
