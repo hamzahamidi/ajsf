95772069f0981b37486c697c3ba9d3123bf64bb8
Naveen Kumar K <37833848+knaveen01@users.noreply.github.com>
2019-05-28 23:06:37 +0530

    Fix for date picker - Date is a day less than the picked date from the date picker form control (#69)
    Fix: Date picker - Date is a day less than the picked date from the date picker form control

--- projects/angular6-json-schema-form/src/lib/shared/date.functions.ts
+++ projects/angular6-json-schema-form/src/lib/shared/date.functions.ts
@@ -57,18 +57,18 @@ export function stringToDate(dateString) {
   const thisYear = +(new Date().getFullYear() + '').slice(-2);
   // Check for [YYYY, MM, DD]
   if (dateParts[0] > 1000 && dateParts[0] < 2100 && dateParts[1] <= 12 && dateParts[2] <= 31) {
-    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);    
   // Check for [MM, DD, YYYY]
   } else if (dateParts[0] <= 12 && dateParts[1] <= 31 && dateParts[2] > 1000 && dateParts[2] < 2100) {
-    return new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);
+    return new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);    
   // Check for [MM, DD, YY]
   } else if (dateParts[0] <= 12 && dateParts[1] <= 31 && dateParts[2] < 100) {
     const year = (dateParts[2] <= thisYear ? 2000 : 1900) + dateParts[2];
-    return new Date(year, dateParts[0] - 1, dateParts[1]);
+    return new Date(year, dateParts[0] - 1, dateParts[1]);    
   // Check for [YY, MM, DD]
   } else if (dateParts[0] < 100 && dateParts[1] <= 12 && dateParts[2] <= 31) {
     const year = (dateParts[0] <= thisYear ? 2000 : 1900) + dateParts[0];
-    return new Date(year, dateParts[1] - 1, dateParts[2]);
+    return new Date(year, dateParts[1] - 1, dateParts[2]);    
   }
   return null;
 }
