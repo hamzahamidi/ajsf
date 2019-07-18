1c3d110bc5e838a1e23cb26b0001af04de6892f2
Justin Early <813090+earlyster@users.noreply.github.com>
2019-07-18 15:44:14 -0700

    removing extra header

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,23 +1,3 @@
-/*-
- * ===============LICENSE_START================================================
- * Acumos Apache-2.0
- * ============================================================================
- * Copyright (C) 2019 Nordix Foundation.
- * ============================================================================
- * This Acumos software file is distributed by Nordix Foundation
- * under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * This file is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ===============LICENSE_END==================================================
- */
-
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
