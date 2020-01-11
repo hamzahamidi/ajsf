f01daf85469e00cdbf127605d7cf92e5e518bc6f
Hamza HAMIDI <hamidihamzacg@gmail.com>
2020-01-11 20:04:43 +0100

    Refactor(Global): divide frameworks

---
+++ projects/ajsf-material/src/public_api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of @ajsf/material-framework
+ */
+
+export * from './lib/material-design.framework';
+export * from './lib/material-design-framework.module';
+export * from './lib/material-design-framework.component';
+export * from './lib/widgets/public_api';
