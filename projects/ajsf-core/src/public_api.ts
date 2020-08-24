689bf98818b707c33069816f16f586840195621c
Bogdan Baghiu <bogdantrabajo@gmail.com>
2020-08-24 09:41:05 +0200

    Add Spanish validation messages

--- projects/ajsf-core/src/public_api.ts
+++ projects/ajsf-core/src/public_api.ts
@@ -8,12 +8,13 @@ export { JsonSchemaFormComponent } from './lib/json-schema-form.component';
 export { Framework } from './lib/framework-library/framework';
 export { FrameworkLibraryService } from './lib/framework-library/framework-library.service';
 export {
+    deValidationMessages,
     enValidationMessages,
+    esValidationMessages,
     frValidationMessages,
     itValidationMessages,
     ptValidationMessages,
-    zhValidationMessages,
-    deValidationMessages,
+    zhValidationMessages
   } from './lib/locale';
 export * from './lib/widget-library';
 export * from './lib/widget-library/widget-library.module';
