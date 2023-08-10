b72d6ed4ca01581adf3ae22087f9d54b68a606c8
zahmo <zuser@nowhere.nw>
2023-08-10 06:32:12 +0200

    build(deps):addition of tailwind config file
    needed incase tailwind needs to be used to help address more replacements to flex-layout
    see https://tailwindcss.com/docs/guides/angular

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{html,ts}",
+        "./projects/**/*.{html,ts}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
-
