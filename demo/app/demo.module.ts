f89e9ae0b08bc493c2e5faa75f7204d267132a66
tof <cvanderslagmolen@yahoo.fr>
2024-02-08 01:34:46 +0100

    remove flex

--- demo/app/demo.module.ts
+++ demo/app/demo.module.ts
@@ -1,41 +1,48 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { AceEditorDirective } from './ace-editor.directive';
-import { DemoComponent } from './demo.component';
-import { DemoRootComponent } from './demo-root.component';
-import { routes } from './demo.routes';
-import { JsonSchemaFormModule } from '@ajsf/core';
-import { Bootstrap3FrameworkModule } from '@ajsf/bootstrap3';
-import { Bootstrap4FrameworkModule } from '@ajsf/bootstrap4';
-import { Bootstrap5FrameworkModule } from '@ajsf/bootstrap5';
-import { MaterialDesignFrameworkModule } from '@ajsf/material';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSelectModule } from "@angular/material/select";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { AceEditorDirective } from "./ace-editor.directive";
+import { DemoComponent } from "./demo.component";
+import { DemoRootComponent } from "./demo-root.component";
+import { routes } from "./demo.routes";
+import { JsonSchemaFormModule } from "@ajsf/core";
+import { Bootstrap3FrameworkModule } from "@ajsf/bootstrap3";
+import { Bootstrap4FrameworkModule } from "@ajsf/bootstrap4";
+import { Bootstrap5FrameworkModule } from "@ajsf/bootstrap5";
+import { MaterialDesignFrameworkModule } from "@ajsf/material";
 
 @NgModule({
   declarations: [AceEditorDirective, DemoComponent, DemoRootComponent],
   imports: [
-    BrowserModule, BrowserAnimationsModule, FlexLayoutModule, FormsModule,
-    HttpClientModule, MatButtonModule, MatCardModule, MatCheckboxModule,
-    MatIconModule, MatMenuModule, MatSelectModule, MatToolbarModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" }),
     Bootstrap3FrameworkModule,
     Bootstrap4FrameworkModule,
     Bootstrap5FrameworkModule,
     MaterialDesignFrameworkModule,
-    JsonSchemaFormModule
+    JsonSchemaFormModule,
   ],
-  bootstrap: [DemoRootComponent]
+  bootstrap: [DemoRootComponent],
 })
-
-export class DemoModule { }
+export class DemoModule {}
