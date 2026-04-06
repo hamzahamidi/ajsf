63d1719aae0b2bbf57fa7f121ce27ffac30a88ed
cvs <cvanderslagmolen@yahoo.fr>
2026-04-06 11:54:28 +0200

    migration

--- demo-bootstrap5/main.ts
+++ demo-bootstrap5/main.ts
@@ -1,4 +1,4 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {environment} from './environments/environment';
@@ -19,6 +19,7 @@ if (environment.production) {
 
 bootstrapApplication(DemoRootComponent, {
   providers: [
+    provideZoneChangeDetection(),
     importProvidersFrom(
       BrowserModule,
       FormsModule,
